f6853a32dd2b27f21acd439dc85780ed90d30931
ZealousGeneticist <126611817+ZealousGeneticist@users.noreply.github.com>
2023-11-03 14:44:11 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cartoflow
+Currently Under construction. It currently has massive errors being fixed as we speak. Please don't use yet.
--- cartoflow.sh
+++ cartoflow.sh
@@ -2,7 +2,7 @@
 ##Make sure to run:
 #chmod u+x cartoflow.sh
 ##Then run at least:
-#bash cartoflow.sh
+#bash cartoflow.sh -i <your_chemical_file.tsv>
 ##Workflow Program which runs all other Programs##
 ##################################
 #Define Input and Output Files
@@ -27,22 +27,22 @@ while [[ $# -gt 0 ]]; do
     case "$1" in
         -a)
             if [ -n "$2" ]; then
-                GENEBRIDGE_ARGS="$GENEBRIDGE_ARGS -a '$2'"
+                GENEBRIDGE_ARGS="$GENEBRIDGE_ARGS -a $2"
                 fileName="$2"
             fi
             shift 2
             ;;
         -b)
             if [ -n "$2" ]; then
-                GENEBRIDGE_ARGS="$GENEBRIDGE_ARGS -b '$2'"
+                GENEBRIDGE_ARGS="$GENEBRIDGE_ARGS -b $2"
                 fileName2="$2"
             fi
             shift 2
             ;;
         -c)
             if [ -n "$2" ]; then
-                CARTOGENE_ARGS="$CARTOGENE_ARGS -c '$2'"
-                GENEBRIDGE_ARGS="$GENEBRIDGE_ARGS -c '$2'"
+                CARTOGENE_ARGS="$CARTOGENE_ARGS -c $2"
+                GENEBRIDGE_ARGS="$GENEBRIDGE_ARGS -c $2"
                 outfile1="$2"
                 outfile1+="_chemical-protein.tsv"
             fi
@@ -50,64 +50,64 @@ while [[ $# -gt 0 ]]; do
             ;;
         -d)
             if [ -n "$2" ]; then
-                CARTOGENE_ARGS="$CARTOGENE_ARGS -d '$2'"
+                CARTOGENE_ARGS="$CARTOGENE_ARGS -d $2"
                 debug="$2"
             fi
             shift 2
             ;;
         -e)
             if [ -n "$2" ]; then
-                GENEBRIDGE_ARGS="$GENEBRIDGE_ARGS -e '$2'"
+                GENEBRIDGE_ARGS="$GENEBRIDGE_ARGS -e $2"
                 outputHeader="$2"
             fi
             shift 2
             ;;
         -f)
             if [ -n "$2" ]; then
-                GENEBRIDGE_ARGS="$GENEBRIDGE_ARGS -f '$2'"
+                GENEBRIDGE_ARGS="$GENEBRIDGE_ARGS -f $2"
                 chugReduction="$2"
             fi
             shift 2
             ;;
         -g)
             if [ -n "$2" ]; then
-                CARTOGENE_ARGS="$CARTOGENE_ARGS -g '$2'"
-                GENEBRIDGE_ARGS="$GENEBRIDGE_ARGS -g '$2'"
+                CARTOGENE_ARGS="$CARTOGENE_ARGS -g $2"
+                GENEBRIDGE_ARGS="$GENEBRIDGE_ARGS -g $2"
                 organism="$2"
             fi
             shift 2
             ;;
         -i)
             if [ -n "$2" ]; then
-                CARTOGENE_ARGS="$CARTOGENE_ARGS -i '$2'"
+                CARTOGENE_ARGS="$CARTOGENE_ARGS -i $2"
                 infile="$2"
             fi
             shift 2
             ;;
         -j)
             if [ -n "$2" ]; then
-                CARTOGENE_ARGS="$CARTOGENE_ARGS -j '$2'"
+                CARTOGENE_ARGS="$CARTOGENE_ARGS -j $2"
                 outjson="$2"
             fi
             shift 2
             ;;
         -o)
             if [ -n "$2" ]; then
-                CARTOGENE_ARGS="$CARTOGENE_ARGS -o '$2'"
+                CARTOGENE_ARGS="$CARTOGENE_ARGS -o $2"
                 outfile3="$2"
             fi
             shift 2
             ;;
         -r)
             if [ -n "$2" ]; then
-                CARTOGENE_ARGS="$CARTOGENE_ARGS -r '$2'"
+                CARTOGENE_ARGS="$CARTOGENE_ARGS -r $2"
                 removeJSON="$2"
             fi
             shift 2
             ;;
         -t)
             if [ -n "$2" ]; then
-                CARTOGENE_ARGS="$CARTOGENE_ARGS -t '$2'"
+                CARTOGENE_ARGS="$CARTOGENE_ARGS -t $2"
                 test="$2"
             fi
             shift 2
@@ -125,6 +125,7 @@ cd cartoflow
 git clone https://github.com/ZealousGeneticist/cartogene.git
 git clone https://github.com/bagrow/linkcomm.git
 git clone https://github.com/ZealousGeneticist/genebridge.git
+echo "If git wasn't 'found', then you may have to run this:\nsudo apt install git"
 #
 cd ./cartogene
 read -p "Please DO NOT CONTINUE until your chemical list input file (up to cartogene specifications of course) is placed in the cartogene respository."
